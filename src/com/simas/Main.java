d4509f50ad71637dd6ef74a1694168ba420d3fde
Simas <ab.simas@gmail.com>
2017-05-20 21:57:31 +0300

    Add communication through a message example.

---
+++ src/com/simas/Main.java
@@ -0,0 +1,30 @@
+package com.simas;
+
+import com.simas.processes.Process;
+import com.simas.resources.Message;
+import com.simas.resources.Resource;
+
+public class Main {
+
+  public static void main(String[] args) throws InterruptedException {
+
+  }
+
+  /**
+   * How processes A and B communicate with a message resource.
+   */
+  private static void abCommunication() throws InterruptedException {
+    // Dummy processes
+    final Process a = null, b = null;
+
+    // b waits for a message // Blocking call
+    final Message message = Resource.MESSAGE.request(b, element -> (element.destination == b));
+
+    // a creates a message for b // Will unblock the previous wait
+    Resource.MESSAGE.create(a, element -> element.destination = b);
+
+    // b frees the message // This is unnecessary because messages are consumed after being returned by a request
+    message.free();
+  }
+
+}
