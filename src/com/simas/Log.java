e6c2e8cc010b888159eb73b31e13a02f6bb1633b
Simas <ab.simas@gmail.com>
2017-05-20 21:52:30 +0300

    Convenience logger class.

---
+++ src/com/simas/Log.java
@@ -0,0 +1,41 @@
+package com.simas;
+
+/**
+ * Created by Simas on 2017 May 20.
+ */
+public class Log {
+
+  /**
+   * Private c-tor
+   */
+  private Log() {}
+
+  /**
+   * Verbose
+   */
+  public static void v(String string, Object... format) {
+    System.out.println(String.format(string, format));
+  }
+
+  /**
+   * Debug
+   */
+  public static void d(String string, Object... format) {
+    System.out.println(String.format(string, format));
+  }
+
+  /**
+   * Warning
+   */
+  public static void w(String string, Object... format) {
+    System.out.println(String.format(string, format));
+  }
+
+  /**
+   * Error
+   */
+  public static void e(String string, Object... format) {
+    System.out.println(String.format(string, format));
+  }
+
+}
