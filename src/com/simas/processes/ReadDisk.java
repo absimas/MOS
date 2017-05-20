ec5d1161d9a65c4cbf2cc9d7db6a9ed714177178
Simas <ab.simas@gmail.com>
2017-05-21 00:02:05 +0300

    Create initial implementations for Read/Write disk.

--- src/com/simas/processes/ReadDisk.java
+++ src/com/simas/processes/ReadDisk.java
@@ -1,10 +1,14 @@
 package com.simas.processes;
 
+import com.simas.resources.DiskPacket;
 import com.simas.resources.Element;
+import com.simas.resources.Message;
+import com.simas.resources.Resource;
+import com.simas.resources.StringElement;
 import com.sun.istack.internal.Nullable;
 
 /**
- * Process that read from disk.
+ * Process that reads from disk.
  */
 public class ReadDisk extends Process {
 
@@ -16,7 +20,22 @@ public class ReadDisk extends Process {
 
   @Override
   public void run() {
+    // Wait for disk read resource
+    final DiskPacket packet = Resource.DISK_READ_PACKET.request(this);
 
+    // Wait for 3rd channel resource // ToDo element type?
+    final StringElement channel3 = Resource.CHANNEL_3.request(this);
+
+    // ToDo read from channel3
+
+    // Free 3rd channel
+    channel3.free();
+
+    // Send message to packet creator
+    Resource.MESSAGE.create(this, element -> {
+      element.destination = packet.creator;
+      element.message = "Reading done";
+    });
   }
 
 }
--- src/com/simas/processes/WriteDisk.java
+++ src/com/simas/processes/WriteDisk.java
@@ -1,6 +1,9 @@
 package com.simas.processes;
 
+import com.simas.resources.DiskPacket;
 import com.simas.resources.Element;
+import com.simas.resources.Resource;
+import com.simas.resources.StringElement;
 import com.sun.istack.internal.Nullable;
 
 /**
@@ -16,7 +19,22 @@ public class WriteDisk extends Process {
 
   @Override
   public void run() {
+    // Wait for disk write resource
+    final DiskPacket packet = Resource.DISK_WRITE_PACKET.request(this);
 
+    // Wait for 3rd channel resource // ToDo element type?
+    final StringElement channel3 = Resource.CHANNEL_3.request(this);
+
+    // ToDo write to channel3
+
+    // Free 3rd channel
+    channel3.free();
+
+    // Send message to packet creator
+    Resource.MESSAGE.create(this, element -> {
+      element.destination = packet.creator;
+      element.message = "Writing done";
+    });
   }
 
 }
