45f8462a521edcdaae7cb7ebf80cdbce1816e83f
Simas <ab.simas@gmail.com>
2017-05-21 18:09:29 +0300

    Add convenience methods from VRM.

--- src/com/simas/Utils.java
+++ src/com/simas/Utils.java
@@ -35,4 +35,28 @@ public class Utils {
     return string == null || string.isEmpty();
   }
 
+  /**
+   * Precedes the given string with zeroes until the wanted size is met.
+   * Note that if the given string is longer, it will be trimmed to meet the size and no zeroes will be added.
+   * E.g.
+   * <code>
+   *   precedeZeroes("abc", 5);
+   * </code>
+   * will return 00abc.
+   * @param string  string that will be preceded by zeroes
+   * @param size    total size of the resulting string
+   * @return string preceded with zeroes to meet the wanted size.
+   */
+  public static String precedeZeroes(String string, int size) {
+    string = "00000000000000" + string;
+    return string.substring(string.length()-size);
+  }
+
+  /**
+   * @see #precedeZeroes(String, int)
+   */
+  public static String precedeZeroes(int integer, int size) {
+    return precedeZeroes(String.valueOf(integer), size);
+  }
+
 }
